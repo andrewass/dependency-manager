3fd768cf28049d4b2f1e740b5aa9c20179a684c5
Andreas Wassum <a.wassum@gmail.com>
2024-09-30 18:39:18 +0200

    Moved app to frontend folder

---
+++ frontend/src/app/dependencies/page.tsx
@@ -0,0 +1,12 @@
+import {getDependencies} from "@/app/dependencies/actions";
+
+
+export default function DependencyPage(){
+
+    return(
+        <div>
+           hello
+        </div>
+    )
+
+}
