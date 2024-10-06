751b78687205a516fda4330a5a2607120ae248ae
Andreas Wassum <a.wassum@gmail.com>
2024-10-06 22:31:40 +0200

    Setup for nextui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import {Button} from "@nextui-org/react";
 
 export default function Home() {
 
     return (
         <div>
             <p>Main Page</p>
-            <Button>Click Me</Button>
         </div>
     );
 }
